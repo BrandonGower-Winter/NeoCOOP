37049b1f7c619e5bdde428aa4fdcd855ae7ad885
Brandon Gower-Winter <brandongowerwinter@gmail.com>
2022-01-15 21:17:48 +0200

    Updated batchrunner.sh to ony record data every 5 iterations

--- batchrunner.sh
+++ batchrunner.sh
@@ -4,7 +4,7 @@ count=0
 total=0
 while read l; do
   touch "$2/$l-console.log"
-  nohup python3 ./src/main.py -f $1 -lrb "$2/$l" --frequency 1 --seed "$l" >> "$2/$l-console.log" &
+  nohup python3 ./src/main.py -f $1 -lrb "$2/$l" --frequency 5 --seed "$l" >> "$2/$l-console.log" &
   count=$((count+1))
   if [ $count -eq 5 ]; then
       count=0
