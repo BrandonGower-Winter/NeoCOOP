beba0cb6cf38f424db5cf0ecf1eaebcb520926aa
Brandon Gower-Winter <brandongowerwinter@gmail.com>
2022-01-16 09:21:18 +0200

    Updated batchrunner.sh to run 10 simulation per cycle instead of 5

--- batchrunner.sh
+++ batchrunner.sh
@@ -6,10 +6,10 @@ while read l; do
   touch "$2/$l-console.log"
   nohup python3 ./src/main.py -f $1 -lrb "$2/$l" --frequency 5 --seed "$l" >> "$2/$l-console.log" &
   count=$((count+1))
-  if [ $count -eq 5 ]; then
+  if [ $count -eq 10 ]; then
       count=0
       wait
-      total=$((total + 5))
+      total=$((total + 10))
       echo "$total Complete..."
   fi
 done <./resources/seeds.list
