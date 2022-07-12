42b7beaf94f0eaf43acbdad430043d30cde2f024
Brandon Gower-Winter <brandongowerwinter@gmail.com>
2022-07-12 08:22:21 +0200

    Added Modified rainfall_plotter and graph_plotter scripts

--- src/graph_plotter.py
+++ src/graph_plotter.py
@@ -19,7 +19,11 @@ array_size = 2000
 iteration_multiplier = 5
 runs_count = 50
 
-plots_ids = ['N', '1', '2', '4', '8', '16', '32', 'P','16F']
+plots_ids = ['PF', '1F', '2F', '4F', '8F', '16F', '32F', '64F', '128F', 'NF']
+#plots_ids = ['8F', '16F', '24F', '32F', '40F', '48F', '56F', '64F']
+#plots_ids = ['PS', '1S', '2S', '4S', '8S', '16S', '32S', '64S', '128S', 'NS']
+#plots_ids = ['PA', '1A', '2A', '4A', '8A', '16A', '32A', '64A', '128A', 'NA']
+levels = [50.0,55.0,60.0,65.0,70.0,75.0,80.0,85.0,90.0,95.0,100.0]
 
 def get_scenario_data(runs: {}):
 
@@ -29,6 +33,7 @@ def get_scenario_data(runs: {}):
     sub_placeholder = np.zeros((array_size, runs_count), dtype=float)
     gini_pop = np.zeros((array_size, runs_count), dtype=float)
     settlements_pop = np.zeros((array_size, runs_count), dtype=float)
+    attachments = np.zeros((array_size, runs_count), dtype=float)
 
     sub_requests = np.zeros((array_size * iteration_multiplier, runs_count), dtype=float)
     auth_requests = np.zeros((array_size * iteration_multiplier, runs_count), dtype=float)
@@ -41,22 +46,37 @@ def get_scenario_data(runs: {}):
     runs_processed = 1
     for seed in runs:
         for i in range(array_size):
-            placeholder[i][index] = runs[seed]['population']['number'][i]
-            placeholder_pop[i][index] = runs[seed]['population']['total'][i]
-            peer_placeholder[i][index] = runs[seed]['peer_transfer']['mean'][i]
-            sub_placeholder[i][index] = runs[seed]['sub_transfer']['mean'][i]
-            gini_pop[i][index] = np.nan_to_num(runs[seed]['inequality']['gini'][i])
-            settlements_pop[i][index] = runs[seed]['settlements']['count'][i]
 
-            peer_dst_placeholder[i][index] = np.nan_to_num(np.array(runs[seed]['peer_transfer']['dist'][i]))
-            sub_dst_placeholder[i][index] = np.nan_to_num(np.array(runs[seed]['sub_transfer']['dist'][i]))
+            if i < len(runs[seed]['population']['number']):
+                placeholder[i][index] = runs[seed]['population']['number'][i]
+                placeholder_pop[i][index] = runs[seed]['population']['total'][i]
+                peer_placeholder[i][index] = runs[seed]['peer_transfer']['mean'][i]
+                sub_placeholder[i][index] = runs[seed]['sub_transfer']['mean'][i]
+                gini_pop[i][index] = np.nan_to_num(runs[seed]['inequality']['gini'][i])
+                settlements_pop[i][index] = runs[seed]['settlements']['count'][i]
+                attachments[i][index] = runs[seed]['attachment']['mean'][i]
+
+                peer_dst_placeholder[i][index] = np.nan_to_num(np.array(runs[seed]['peer_transfer']['dist'][i]))
+                sub_dst_placeholder[i][index] = np.nan_to_num(np.array(runs[seed]['sub_transfer']['dist'][i]))
+            else:
+                placeholder[i][index] = 0
+                placeholder_pop[i][index] = 0
+                peer_placeholder[i][index] = 0
+                sub_placeholder[i][index] = 0
+                gini_pop[i][index] = 0
+                settlements_pop[i][index] = 0
+                attachments[i][index] = 0
+
+                peer_dst_placeholder[i][index] = np.zeros(10)
+                sub_dst_placeholder[i][index] = np.zeros(10)
+
 
         index += 1
         runs_processed += 1
         if runs_processed > runs_count:
             break
 
-    line_data = np.zeros((25, array_size), dtype=float)
+    line_data = np.zeros((27, array_size), dtype=float)
     bar_data = np.zeros((2, 3, 10), dtype=float)
     stats_data = np.zeros((3,runs_count), dtype=float)
     population_data = placeholder
@@ -112,6 +132,9 @@ def get_scenario_data(runs: {}):
 
         line_data[24][i] = np.mean(gini_pop[i])
 
+        line_data[25][i] = np.mean(attachments[i])
+        line_data[26][i] = np.std(attachments[i])
+
         if i == 0:
             bar_data[0][0] = np.mean(peer_dst_placeholder[i], axis=0)
             bar_data[1][0] = np.mean(sub_dst_placeholder[i], axis=0)
@@ -143,7 +166,7 @@ def get_scenario_data(runs: {}):
 def write_plot(agent_types: [], filename, data, title: str, index: [int], x_axis: str, y_axis: str,
                legend: str = 'lower right', show_std: int = -1, data_types={}, iterations = None):
 
-    fig, ax = pyplot.subplots()
+    fig, ax = pyplot.subplots(dpi=200)
     ax.set_title(title)
     ax.set_xlabel(x_axis)
     ax.set_ylabel(y_axis)
@@ -180,7 +203,7 @@ def write_bar_plot(key, filename, data, title: str, x_axis: str, y_axis: str,
 
     divide_arr = ['beginning', 'middle', 'end']
 
-    fig, ax = pyplot.subplots()
+    fig, ax = pyplot.subplots(dpi=200)
     bins = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0']
     x_bins = np.arange(len(bins))
     ax.set_title(title)
@@ -240,21 +263,21 @@ def main():
     write_plot(plots_ids, '%s/gini/inequality' % parser.output, data,
                '', [24], 'Iteration (Year)', 'Gini Index', legend='upper left')
 
-    peer_heatmaps = np.zeros((len(plots_ids), 7))
-    sub_heatmaps = np.zeros((len(plots_ids), 7))
+    peer_heatmaps = np.zeros((len(plots_ids), 6))
+    sub_heatmaps = np.zeros((len(plots_ids), 6))
 
     for i, id in enumerate(plots_ids):
-        peer_heatmaps[i] = bar_data[id][0][2][1:8]
-        sub_heatmaps[i] = bar_data[id][1][2][1:8]
+        peer_heatmaps[i] = bar_data[id][0][2][2:8]
+        sub_heatmaps[i] = bar_data[id][1][2][2:8]
 
     from scipy.ndimage.filters import gaussian_filter
 
     x_labels = plots_ids
-    y_labels = ['11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80']
+    y_labels = ['21-30', '31-40', '41-50', '51-60', '61-70', '71-80']
 
-    max_labels = np.round(np.linspace(0.0, 0.5, 11), 2)
+    max_labels = np.round(np.linspace(0.0, 0.75, 16), 2)
 
-    fig, ax = pyplot.subplots()
+    fig, ax = pyplot.subplots(dpi=200)
     ax.set_title('')
     ax.set_xlabel('Stress Scenario')
     ax.set_ylabel('Peer Transfer Property (%)')
@@ -263,14 +286,14 @@ def main():
     ax.set_yticklabels(y_labels)
 
     # Generate the pix map
-    plot = ax.contourf(np.transpose(peer_heatmaps), max_labels, cmap='hot')#, vmin=max_labels[i][0], vmax=max_labels[i][1])
+    plot = ax.contourf(np.transpose(peer_heatmaps), max_labels, cmap='hot')
     fig.colorbar(plot)
     ax.set_aspect('auto')
 
     fig.savefig('%s/peer_heatmap' % parser.output)
     pyplot.close(fig)
 
-    fig, ax = pyplot.subplots()
+    fig, ax = pyplot.subplots(dpi=200)
     ax.set_title('')
     ax.set_xlabel('Stress Scenario')
     ax.set_ylabel('Subordinate Transfer Property (%)')
@@ -279,7 +302,7 @@ def main():
     ax.set_yticklabels(y_labels)
 
     # Generate the pix map
-    plot = ax.contourf(np.transpose(sub_heatmaps), max_labels, cmap='bone')#, vmin=max_labels[i][0], vmax=max_labels[i][1])
+    plot = ax.contourf(np.transpose(sub_heatmaps), max_labels, cmap='bone')
     fig.colorbar(plot)
     ax.set_aspect('auto')
 
@@ -294,7 +317,7 @@ def main():
         gini_heatmaps[i] = data[id][24]
 
 
-    fig, ax = pyplot.subplots()
+    fig, ax = pyplot.subplots(dpi=200)
     ax.set_title('')
     ax.set_xlabel('Iterations (Year)')
     ax.set_ylabel('Gini Index')
@@ -309,6 +332,112 @@ def main():
     fig.savefig('%s/gini_heatmap' % parser.output)
     pyplot.close(fig)
 
+    y_labels = plots_ids
+    if 'PF' in y_labels:
+        y_labels.remove('PF')
+    if 'PS' in y_labels:
+        y_labels.remove('PS')
+    if 'PA' in y_labels:
+        y_labels.remove('PA')
+    x_labels = ['0', '', '2000', '', '4000', '', '6000', '', '8000', '', '10000']
+
+    peer_heatmaps = np.zeros((len(y_labels), 2000))
+    sub_heatmaps = np.zeros((len(y_labels), 2000))
+
+    for i, id in enumerate(y_labels):
+        peer_heatmaps[i] = data[id][2]
+        sub_heatmaps[i] = data[id][4]
+
+    #y_labels = [''] + y_labels
+
+    max_labels = np.round(np.linspace(45.0, 55.0, 21), 2)
+
+    fig, ax = pyplot.subplots(dpi=200)
+    ax.set_title('')
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Stress Scenario')
+
+    ax.set_xticklabels(x_labels)
+    ax.set_yticklabels(y_labels)
+
+    # Generate the pix map
+    plot = ax.contourf(peer_heatmaps, cmap='copper')#, interpolation='none')
+    ax.contour(peer_heatmaps, colors = 'k')
+    cbar = fig.colorbar(plot)
+    cbar.set_label('Peer Transfer %', rotation=90)
+    ax.set_aspect('auto')
+    fig.savefig('%s/peer_transfer_heatmap' % parser.output)
+    pyplot.close(fig)
+
+    max_labels = np.round(np.linspace(40.0, 55.0, 31), 2)
+
+    fig, ax = pyplot.subplots(dpi=200)
+    ax.set_title('')
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Stress Scenario')
+
+    ax.set_xticklabels(x_labels)
+    ax.set_yticklabels(y_labels)
+
+    # Generate the pix map
+    plot = ax.contourf(sub_heatmaps, cmap='bone')#, interpolation='none')
+    ax.contour(sub_heatmaps, colors = 'k')
+    cbar = fig.colorbar(plot)
+    cbar.set_label('Sub Transfer %', rotation=90)
+    ax.set_aspect('auto')
+    fig.savefig('%s/sub_transfer_heatmap' % parser.output)
+    pyplot.close(fig)
+
+
+    attachment_heatmaps = np.zeros((len(y_labels), 2000))
+    for i, id in enumerate(y_labels):
+        attachment_heatmaps[i] = data[id][25]
+
+    fig, ax = pyplot.subplots(dpi=200)
+    ax.set_title('')
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Stress Scenario')
+
+    ax.set_xticklabels(x_labels)
+    ax.set_yticklabels(y_labels)
+
+    # Generate the pix map
+    plot = ax.contourf(attachment_heatmaps, cmap='pink')#, interpolation='none')
+    ax.contour(attachment_heatmaps, colors = 'k')
+    cbar = fig.colorbar(plot)
+    cbar.set_label('Attachment', rotation=90)
+    ax.set_aspect('auto')
+    fig.savefig('%s/attachment_heatmap' % parser.output)
+    pyplot.close(fig)
+
+    ax.set_title('')
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Stress Scenario')
+
+    ax.set_xticklabels(x_labels)
+    ax.set_yticklabels(y_labels)
+    pyplot.close(fig)
+
+    fig, ax = pyplot.subplots(dpi=200)
+    # Generate the pix map
+    toPlot = peer_heatmaps - sub_heatmaps
+    plot = ax.contourf(toPlot, cmap='summer', linewidths = 1.0)#, interpolation='none')
+    ax.contour(toPlot, colors = 'k')
+    cbar = fig.colorbar(plot)
+    cbar.set_label('Difference (%)', rotation=90)
+    ax.set_aspect('auto')
+    fig.savefig('%s/difference_heatmap' % parser.output)
+    pyplot.close(fig)
+
+    toPlot = [data[a][2][1999] - data[a][4][1999] for a in y_labels]
+
+    fig, ax = pyplot.subplots(dpi=200)
+    ax.set_xlabel('Stress Scenarios')
+    ax.set_ylabel('Difference (%) (Peer - Sub)')
+    plot = ax.plot(y_labels, toPlot)
+    ax.set_aspect('auto')
+    fig.savefig('%s/difference_plot' % parser.output)
+    pyplot.close(fig)
 
     for a in data:
         write_plot([a], '%s/transfer_chance/%s_transfer_chance' % (parser.output, a), data,
@@ -328,6 +457,10 @@ def main():
                    'Relative Difference in Peer and Sub Transfer properties\n for Scenario %s where Sub[0] < Peer[0]' % a,
                    [16, 18], 'Iteration', '%', data_types=data_types, show_std=1)
 
+        write_plot([a], '%s/attachment/%s_attachment' % (parser.output, a), data,
+                   'Evolution of Attachment Gene for Scenario %s' % a,
+                   [25], 'Iteration', 'Attachment', data_types=data_types, show_std=1)
+
         write_plot([a], '%s/actions/%s_actions' % (parser.output, a), log_data,
                    'Number of Resource Transfer Actions\n for Scenario %s' % a,
                    [0,4], 'Iteration', 'Count', data_types={a: ['Sub', 'Peer']},
@@ -342,7 +475,7 @@ def main():
         print('%s:\n0: %s +/- %s p:%s\t 5000: %s +/- %s p:%s\t10000: %s +/- %s p:%s' % ('Stats:', data[a][0][0], data[a][1][0], data[a][18][0],
                                                                            data[a][0][999], data[a][1][999], data[a][20][999],
                                                                            data[a][0][1999], data[a][1][1999], data[a][20][1999]))
-        print('Ranksums:' + str(stats.ranksums(stats_data[a][1], stats_data[a][2], alternative='two-sided').pvalue))
+        print('Diff: %s Ranksums: %s' % (str(data[a][2][1999] - data[a][4][1999]),str(stats.ranksums(stats_data[a][1], stats_data[a][2], alternative='greater').pvalue)))
         print()
 
 
--- src/rain_fall_plotter.py
+++ src/rain_fall_plotter.py
@@ -11,25 +11,25 @@ def main():
     iterations = numpy.arange(500)
 
     rand = numpy.zeros(500)
-    for _ in range(12):
-        rand += numpy.random.rand(500)
-    rand = rand/12
+    #for _ in range(12):
+        #rand += numpy.random.rand(500)
+    #rand = rand/12
 
-    #rand = numpy.random.rand(500)
+    rand = numpy.random.rand(500)
 
     data = 1.0 - (0.5 * numpy.sin(2 * math.pi * iterations/500 * 4) + 0.5)
-    start_data = 32 + (10 * data)
-    end_data = 0 + (10 * data)
+    start_data = 0.4 + (0.6 * data)
+    end_data = 0.0 + (0.6 * data)
 
     data = start_data + (end_data - start_data) * rand
 
-    iterations = iterations * 25
+    iterations = iterations * 20
 
     #plt.plot(iterations, start_data)
     plt.plot(iterations, data, '0.6')
-    plt.plot(iterations, (end_data + start_data)/2.0, '0.0')
-    plt.ylabel('Average Monthly Rainfall (mm/month)')
-    plt.xlabel('Iterations (Year)')
+    #plt.plot(iterations, (end_data + start_data)/2.0, '0.0')
+    plt.ylabel('Available Resources')
+    plt.xlabel('Iterations')
 
     plt.savefig('rainfall_example.png', format='png')
 
